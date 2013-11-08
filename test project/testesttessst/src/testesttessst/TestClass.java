132a58b1f533f187be37e16ac4667a863737f8f6
danieladz <daniela.dz@gmail.com>
2013-11-08 13:44:52 -0300

    added comments

--- test project/testesttessst/src/testesttessst/TestClass.java
+++ test project/testesttessst/src/testesttessst/TestClass.java
@@ -3,6 +3,9 @@ package testesttessst;
 public class TestClass {
 	private String var = "var var var";
 
+	/**
+	 * added first comments
+	 */
 	private void showVar() {
 		System.out.println("This is var: " + var);
 	}
