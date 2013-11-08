6f970a4755f388de360cacb3c775e72933710257
danieladz <daniela.dz@gmail.com>
2013-11-08 13:48:51 -0300

    adassfd

--- test project/testesttessst/src/testesttessst/TestClass.java
+++ test project/testesttessst/src/testesttessst/TestClass.java
@@ -1,13 +1,5 @@
 package testesttessst;
 
 public class TestClass {
-	private String var = "var var var";
-
-	/**
-	 * added first comments
-	 */
-	private void showVar() {
-		System.out.println("This is var: " + var);
-	}
 
 }
