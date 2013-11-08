c4f6effe212ba760df5e5b10fb0cc3d7de48015e
danieladz <daniela.dz@gmail.com>
2013-11-08 13:49:48 -0300

    gghhhgg

--- test project/testesttessst/src/testesttessst/TestClass.java
+++ test project/testesttessst/src/testesttessst/TestClass.java
@@ -1,5 +1,13 @@
 package testesttessst;
 
 public class TestClass {
+	private String var = "var var var";
+
+	/**
+	 * added first comments
+	 */
+	private void showVar() {
+		System.out.println("This is var: " + var);
+	}
 
 }
